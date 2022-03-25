abbedc8e07a0fe8d1f36a18d332e99e10496b7b1
juneseok0107@naver.com <juneseok0107@naver.com>
2022-03-26 01:43:09 +0900

    [update] 버그 수정

--- front/src/components/Navigation/Modal.jsx
+++ front/src/components/Navigation/Modal.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './Modal.css'
 
 const Modal = (props) => {
-<<<<<<< HEAD
   const { open, close } = props;
 
   return (
@@ -61,72 +60,6 @@ const Modal = (props) => {
 
   )
   
-=======
-    const { open,close,header} = props;
-    
-    
-    return (
-    <div className={open ? 'openModal modal' : 'modal'}>
-        {open ? (
-          <section>
-
-            {/* <header>
-              {header}
-              <button className="close" onClick={close}>
-                &times;
-              </button>
-            </header> */}
-
-            <main>
-                {props.children}
-                <div className='modalCount'>
-                    <p className='modalAdult'>성인</p>
-                    <div className='modalCountNumber'>
-                        <button id='decrease'>-</button>
-                        <p id='number'>0</p>
-                        <button id='increase'>+</button>
-                    </div>
-                </div>
-
-                <div className='modalCount'>
-                    <p className='modalAdult'>어린이</p>
-                    <div className='modalCountNumber'>
-                        <button id='decrease'>-</button>
-                        <p id='number'>0</p>
-                        <button id='increase'>+</button>
-                    </div>
-                </div>
-
-                <div className='modalCount'>
-                    <p className='modalAdult'>유아</p>
-                    <div className='modalCountNumber'>
-                        <button id='decrease'>-</button>
-                        <p id='number'>0</p>
-                        <button id='increase'>+</button>
-                    </div>
-                </div>
-
-                <div className='modalCount'>
-                    <p className='modalAdult'>반려동물</p>
-                    <div className='modalCountNumber'>
-                        <button id='decrease'>-</button>
-                        <p id='number'>0</p>
-                        <button id='increase'>+</button>
-                    </div>
-                </div>
-            </main>
-
-            <footer>
-              <button className="close" onClick={close}>
-                close
-              </button>
-            </footer>
-
-          </section>
-        ) : null}
-    </div>
-  )
->>>>>>> 5d6ac59dafbf5ef724367953b31955f0bc10f066
 }
 
 export default Modal
