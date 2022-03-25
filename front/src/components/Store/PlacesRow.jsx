7ee2b30b5e807adb118fd43e144315e7363e3301
yeeahG <yeji.sep.k@gmail.com>
2022-03-25 14:56:56 +0900

    [upload]Image hover

--- front/src/components/Store/PlacesRow.jsx
+++ front/src/components/Store/PlacesRow.jsx
@@ -11,20 +11,24 @@ const PlacesRow = (props) => {
         </div>
         
         <div className='rowContent'>
-            <div className='styleText'>
+            {/*<div className='styleText'>
                 <div className='placeName'><h1>{props.name}name</h1></div>
                 <div className='placeDesc'>{props.description}desc</div>
-            </div>
-            <a href="../Layout/Place/서귀포">
+            </div>*/}
+
+            <a href="../Layout/Place/서귀포" className='placesBox'>
                 <img className='placesRow' src="https://cdn.pixabay.com/photo/2016/10/06/17/28/architecture-1719526_960_720.jpg"/>
+                <p className='placesText'>서귀포</p>
             </a>
 
-            <a href="../Layout/Place/시내동">
+            <a href="../Layout/Place/시내동" className='placesBox'>
                 <img className='placesRow' src="https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070__340.jpg"/>
+                <p className='placesText'>시내동</p>
             </a>
             
-            <a href="../Layout/Place/조천읍">
-            <img className='placesRow' src="https://cdn.pixabay.com/photo/2016/12/30/07/59/kitchen-1940174__340.jpg"/>
+            <a href="../Layout/Place/조천읍" className='placesBox'>
+                <img className='placesRow' src="https://cdn.pixabay.com/photo/2016/12/30/07/59/kitchen-1940174__340.jpg"/>
+                <p className='placesText'>조천읍</p>
             </a>
 
         </div>
