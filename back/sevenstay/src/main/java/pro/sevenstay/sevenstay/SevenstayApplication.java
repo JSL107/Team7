ef6e53a19bfe54cf7828a0bbd2279534fb0f41af
JSL107 <juneseok0107@naver.com>
2022-03-29 00:52:44 +0900

    [upload] Accommodation.java

--- back/sevenstay/src/main/java/pro/sevenstay/sevenstay/SevenstayApplication.java
+++ back/sevenstay/src/main/java/pro/sevenstay/sevenstay/SevenstayApplication.java
@@ -1,12 +1,10 @@
 package pro.sevenstay.sevenstay;
 
 // Batch import
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableBatchProcessing // Batch 사용을 위한 어노테이션 추가
 @SpringBootApplication
 public class SevenstayApplication {
 
@@ -15,10 +13,3 @@ public class SevenstayApplication {
 	}
 
 }
-/* 
-cmd /C ""C:\Program Fi
-les\Java\jdk1.8.0_201\bin\java.exe" -agentlib:jdwp=transport=dt_socket,ser
-ver=n,suspend=y,address=localhost:10646 -cp C:\Users\home\AppData\Local\Te
-mp\cp_128bvj5lg2nhm82fjm3pnbvt1.jar pro.sevenstay.sevenstay.SevenstayAppli
-cation "
-*/
--- back/sevenstay/src/main/java/pro/sevenstay/sevenstay/model/dto/AccommodationDTO.java
+++ back/sevenstay/src/main/java/pro/sevenstay/sevenstay/model/dto/AccommodationDTO.java
@@ -1,69 +1,65 @@
 package pro.sevenstay.sevenstay.model.dto;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-// import javax.persistence.Table;
+import pro.sevenstay.sevenstay.model.entity.Accommodation;
 
-@Entity
 public class AccommodationDTO {
-    @Column
+    
+    private String id;
     private String city; // 시 , 도시위치
-    @Column
     private String accommodationName; // 숙소명
-    @Column
     private String address; // 주소
-    @Id
-    @Column
     private String phoneNumber; // 전화번호
 
-    public AccommodationDTO(String city, String accommodationName, String address, String phoneNumber) {
-        this.city = city;
-        this.accommodationName = accommodationName;
-        this.address = address;
-        this.phoneNumber = phoneNumber;
+    public AccommodationDTO(Accommodation accommodation) {
+        this.id = accommodation.getId();
+        this.city = accommodation.getCity();
+        this.accommodationName = accommodation.getAccommodationName();
+        this.address = accommodation.getAddress();
+        this.phoneNumber = accommodation.getPhoneNumber();
     }
 
     
+    public String getId() {
+        return id;
+    }
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
     public String getCity() {
         return city;
     }
-
 
     public void setCity(String city) {
         this.city = city;
     }
 
-
     public String getAccommodationName() {
         return accommodationName;
     }
-
 
     public void setAccommodationName(String accommodationName) {
         this.accommodationName = accommodationName;
     }
 
-
     public String getAddress() {
         return address;
     }
-
 
     public void setAddress(String address) {
         this.address = address;
     }
 
-
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
-
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
-
 
     @Override
     public String toString() {
@@ -71,6 +67,4 @@ public class AccommodationDTO {
                 + ", phoneNumber=" + phoneNumber + "]";
     }
 
-    
 }
-
