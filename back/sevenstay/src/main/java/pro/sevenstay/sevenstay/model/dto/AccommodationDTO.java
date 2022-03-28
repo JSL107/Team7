be8868f675ecbbfd82632f850cf5138c47f80ed3
BanCheolJun <cjfwns528@naver.com>
2022-03-28 22:14:58 +0900

    [upload] 왜 안되지?

--- back/sevenstay/src/main/java/pro/sevenstay/sevenstay/model/dto/AccommodationDTO.java
+++ back/sevenstay/src/main/java/pro/sevenstay/sevenstay/model/dto/AccommodationDTO.java
@@ -1,11 +1,21 @@
 package pro.sevenstay.sevenstay.model.dto;
 
-public class AccommodationDTO {
-    private String city; // 시 , 도시위치
-    private String accommodationName; // 숙소명
-    private String address; // 주소
-    private String phoneNumber; // 전화번호
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+// import javax.persistence.Table;
 
+@Entity
+public class AccommodationDTO {
+    @Column
+    private String city; // 시 , 도시위치
+    @Column
+    private String accommodationName; // 숙소명
+    @Column
+    private String address; // 주소
+    @Id
+    @Column
+    private String phoneNumber; // 전화번호
 
     public AccommodationDTO(String city, String accommodationName, String address, String phoneNumber) {
         this.city = city;
@@ -14,7 +24,7 @@ public class AccommodationDTO {
         this.phoneNumber = phoneNumber;
     }
 
-
+    
     public String getCity() {
         return city;
     }
--- back/sevenstay/src/test/java/pro/sevenstay/sevenstay/SevenstayApplicationTests.java
+++ back/sevenstay/src/test/java/pro/sevenstay/sevenstay/SevenstayApplicationTests.java
@@ -1,7 +1,16 @@
 package pro.sevenstay.sevenstay;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import pro.sevenstay.sevenstay.model.dto.AccommodationDTO;
+
 
 @SpringBootTest
 class SevenstayApplicationTests {
@@ -10,4 +19,42 @@ class SevenstayApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+
+		/*
+		this.city = city;
+        this.accommodationName = accommodationName;
+        this.address = address;
+        this.phoneNumber = phoneNumber; 
+		*/
+
+	@Test
+	public void testSave() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("sevenstay");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		
+		AccommodationDTO ac1 = new AccommodationDTO("제주시", "sevenstay", "어디든", "000-000-0000");
+		em.persist(ac1);
+
+		tx.commit();
+	}
+
+	@Test
+	public void testTttt() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("sevenstay");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+		AccommodationDTO accommo1 = new AccommodationDTO("제주시", "먼호텔", "제주도", "064-796-9999");
+		em.persist(accommo1);
+
+		tx.commit();
+		
+	}
 }
